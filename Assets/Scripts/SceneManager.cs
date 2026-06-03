433f9bfa9903622282b0652b4a0010e92d8b3def
matchagatcha <156051735+matchagatcha@users.noreply.github.com>
2026-06-03 20:07:44 +0800

    linked Affinity button to CoralIdentify scene

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -35,4 +35,8 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+    public void OpenIdentifyScene()
+    {
+        SceneManager.LoadScene("CoralIdentify");
+    }
 }
