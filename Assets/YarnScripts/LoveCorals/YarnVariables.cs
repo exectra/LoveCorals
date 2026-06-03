9ebb60807d790b2af4a986073f45d42d38943846
curryco <84174674+curryco@users.noreply.github.com>
2026-06-03 23:08:52 +0800

    put dialogue system in GameScene

---
+++ Assets/YarnScripts/LoveCorals/YarnVariables.cs
@@ -0,0 +1,11 @@
+using Yarn.Unity;
+
+[System.CodeDom.Compiler.GeneratedCode("YarnSpinner", "3.1.4.0")]
+public partial class YarnVariables : Yarn.Unity.InMemoryVariableStorage, Yarn.Unity.IGeneratedVariableStorage {
+    // Accessor for Number $SWaffinity
+    public float SWaffinity {
+        get => this.GetValueOrDefault<float>("$SWaffinity");
+        set => this.SetValue<float>("$SWaffinity", value);
+    }
+
+}
