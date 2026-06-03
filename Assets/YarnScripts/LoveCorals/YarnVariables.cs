defb5194d97bcd5ff1ddb236cc0cbf80545cdec6
curryco <84174674+curryco@users.noreply.github.com>
2026-06-04 01:10:45 +0800

    changing affinity between yarn and c#

--- Assets/YarnScripts/LoveCorals/YarnVariables.cs
+++ Assets/YarnScripts/LoveCorals/YarnVariables.cs
@@ -1,11 +1,14 @@
-using Yarn.Unity;
+namespace Yarn.Unity.Samples {
 
-[System.CodeDom.Compiler.GeneratedCode("YarnSpinner", "3.1.4.0")]
-public partial class YarnVariables : Yarn.Unity.InMemoryVariableStorage, Yarn.Unity.IGeneratedVariableStorage {
-    // Accessor for Number $SWaffinity
-    public float SWaffinity {
-        get => this.GetValueOrDefault<float>("$SWaffinity");
-        set => this.SetValue<float>("$SWaffinity", value);
+    using Yarn.Unity;
+
+    [System.CodeDom.Compiler.GeneratedCode("YarnSpinner", "3.1.4.0")]
+    public partial class YarnVariables : Yarn.Unity.InMemoryVariableStorage, Yarn.Unity.IGeneratedVariableStorage {
+        // Accessor for Number $SWaffinity
+        public float SWaffinity {
+            get => this.GetValueOrDefault<float>("$SWaffinity");
+            set => this.SetValue<float>("$SWaffinity", value);
+        }
+
     }
-
 }
