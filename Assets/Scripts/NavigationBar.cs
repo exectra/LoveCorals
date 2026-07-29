db1726dc3dd1e13f446e114f4e9f1c93c623cee5
exectra <157939496+exectra@users.noreply.github.com>
2026-07-29 21:07:39 +0800

    added new assets and changed the identifycorals scene
    - nav bar fixed for identify corals scene

--- Assets/Scripts/NavigationBar.cs
+++ Assets/Scripts/NavigationBar.cs
@@ -7,57 +7,47 @@ public class NavigationBar : MonoBehaviour
     public class Tab
     {
         public Button button;
-
         public Image icon;
         public RectTransform iconTransform;
-
         public Sprite normalSprite;
         public Sprite selectedSprite;
-
         public GameObject panel;
     }
 
     [SerializeField] private Tab[] tabs;
-    [SerializeField] private int defaultTab = 0;
-
+    [SerializeField] private int defaultTab = -1;
     [SerializeField] private AudioClip clickSFX;
-    private bool initialize = false;
 
+    private bool initialize = false;
     private int currentTab = -1;
 
     private void Start()
     {
-        Debug.Log(PlayerPrefs.GetInt("GoToCustom"));
-
         for (int i = 0; i < tabs.Length; i++)
         {
             int index = i;
-
             tabs[i].button.onClick.AddListener(() => OpenTab(index));
+
+            if (tabs[i].panel != null) tabs[i].panel.SetActive(false);
+            tabs[i].icon.sprite = tabs[i].normalSprite;
+            tabs[i].iconTransform.localScale = Vector3.one;
         }
 
-
-
-        if (PlayerPrefs.GetInt("GoToCustom") == 1)
-        {
-            OpenTab(2);
-            initialize = true;
-        }
-        else
+        if (defaultTab >= 0 && defaultTab < tabs.Length)
         {
             OpenTab(defaultTab);
-            initialize = true;
         }
 
-        PlayerPrefs.DeleteAll();
-        PlayerPrefs.Save();
+        initialize = true;
     }
+
     public void OpenTab(int index)
     {
         if (currentTab == index)
             return;
 
-        if (initialize && clickSFX != null)
+        // CHANGED: added AudioManager.Instance != null check
+        if (initialize && clickSFX != null && AudioManager.Instance != null)
         {
             AudioManager.Instance.PlaySFX(clickSFX);
         }
@@ -65,16 +55,9 @@ public class NavigationBar : MonoBehaviour
         for (int i = 0; i < tabs.Length; i++)
         {
             bool selected = i == index;
-
             if (tabs[i].panel != null) tabs[i].panel.SetActive(selected);
-
-            tabs[i].icon.sprite = selected
-                ? tabs[i].selectedSprite
-                : tabs[i].normalSprite;
-
-            tabs[i].iconTransform.localScale = selected
-                ? Vector3.one * 1.2f
-                : Vector3.one;
+            tabs[i].icon.sprite = selected ? tabs[i].selectedSprite : tabs[i].normalSprite;
+            tabs[i].iconTransform.localScale = selected ? Vector3.one * 1.2f : Vector3.one;
         }
 
         currentTab = index;
